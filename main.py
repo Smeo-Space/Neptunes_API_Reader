ffb0d7d1b9aa1d584210d9bf509e9d6fc7dcfe8c
Sam Tyler <samtyler905@gmail.com>
2025-11-11 22:01:57 +0000

    Registered all stars (not used in UI yet)

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import sys
 import random
 import time
 import json
-
 import api
+from models import Star
 from window import WindowManager
+
+
 wm = WindowManager()
 
-wm.load()
-
-user = 0
+user = 2
 game = 5685
 
 json = api.get(user,game)
-wm.update_map(json["stars"])
 
+for _, star_data in json['stars'].items():
+    if star_data is None:
+        continue
+
+    Star(star_data)
+
+wm.load()
+wm.update_map(json["stars"])
 wm.app_exec()
